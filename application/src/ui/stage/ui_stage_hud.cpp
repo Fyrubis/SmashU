0d900b93953b2feb2e50142b5c2f6759f29ba91b
Meven <meven.sm3416@gmail.com>
2025-06-19 00:44:41 +0200

    Création de case (LIVES)
    Percent propre
    Importer heart undertale
    Mettre coeur *(liveCount - fallCount) - initialisation
    Mettre coeur soit plein soit vide - update

--- application/src/ui/stage/ui_stage_hud.cpp
+++ application/src/ui/stage/ui_stage_hud.cpp
@@ -7,7 +7,7 @@
 #include "ui/stage/ui_stage_hud.h"
 #include "scene_manager/stage_manager.h"
 
-UIStageHUD::UIStageHUD(Scene *scene)
+UIStageHUD::UIStageHUD(Scene* scene)
     : UIObject(scene)
     , m_damageTexts()
     , m_timeText(nullptr)
@@ -15,50 +15,82 @@ UIStageHUD::UIStageHUD(Scene *scene)
     SetName("Stage HUD");
     SetFadeChildren(false);
 
-    AssetManager *assets = scene->GetAssetManager();
-    SpriteSheet *spriteSheet = nullptr;
-    SpriteGroup *spriteGroup = nullptr;
-    TTF_Font *font = nullptr;
+    AssetManager* assets = scene->GetAssetManager();
+    SpriteSheet* spriteSheet = nullptr;
+    SpriteGroup* spriteGroup = nullptr;
+    TTF_Font* font = nullptr;
 
     int playerCount = g_gameCommon.playerCount;
 
     //--------------------------------------------------------------------------
     // Grid layout
-
-    UIGridLayout *hLayout = new UIGridLayout(m_scene, 1, playerCount);
-    hLayout->SetParent(this);
-    hLayout->SetAnchor(Anchor::SOUTH);
-    hLayout->SetPadding(10.f);
-    hLayout->SetColumnSpacing(50.f);
-    hLayout->SetRowSize(30.f);
-    hLayout->SetColumnSize(80.f);
+    UIGridLayout* hLayout;
+    switch (g_gameCommon.stageConfig.mode)
+    {
+    default:
+    case StageConfig::Mode::LIMITED_LIVES:
+        hLayout = new UIGridLayout(m_scene, 1, playerCount * 2);
+        hLayout->SetParent(this);
+        hLayout->SetAnchor(Anchor::SOUTH);
+        hLayout->SetPadding(10.f);
+        hLayout->SetColumnSpacing(12.5f);
+        hLayout->SetRowSize(30.f);
+        for (int i = 0; i < playerCount; i++) {
+            hLayout->SetColumnSize(i*2, 70.f);  // Percent
+            hLayout->SetColumnSize(i*2 +1, 30.f); // Lives
+            if(i*2 -1 > 0)
+                hLayout->SetColumnSpacing(i*2 -1, 60.f);
+        }
+        break;
+    case StageConfig::Mode::LIMITED_TIME:
+        hLayout = new UIGridLayout(m_scene, 1, playerCount);
+        hLayout->SetParent(this);
+        hLayout->SetAnchor(Anchor::SOUTH);
+        hLayout->SetPadding(10.f);
+        hLayout->SetColumnSpacing(50.f);
+        hLayout->SetRowSize(30.f);
+        hLayout->SetColumnSize(80.f);
+        break;
+    }
 
     //--------------------------------------------------------------------------
     // Informations sur les joueurs
 
-    for (int i = 0; i < playerCount; i++)
+
+    spriteSheet = assets->GetSpriteSheet(SHEET_UI);
+    AssertNew(spriteSheet);
+    spriteGroup = spriteSheet->GetGroup("PlayerBorder");
+    AssertNew(spriteGroup);
+
+    UIImage* fillImage;
+    switch (g_gameCommon.stageConfig.mode)
     {
-        SpriteSheet *spriteSheet = assets->GetSpriteSheet(SHEET_UI);
-        AssertNew(spriteSheet);
-        SpriteGroup *spriteGroup = spriteSheet->GetGroup("PlayerBorder");
-        AssertNew(spriteGroup);
+    default:
+    case StageConfig::Mode::LIMITED_LIVES:
 
-        UIImage *fillImage = new UIImage(m_scene, spriteGroup, 1);
-        fillImage->SetBorders(6, 6, 6, 6, 4.f);
-        fillImage->SetOpacity(0.5f);
-        fillImage->SetLayer(LAYER_UI_BACKGROUND);
-        fillImage->SetParent(this);
+        for (int j = 0; j < playerCount * 2; j++) {
+            fillImage = new UIImage(m_scene, spriteGroup, 1);
+            fillImage->SetBorders(6, 6, 6, 6, 4.f);
+            fillImage->SetOpacity(0.5f);
+            fillImage->SetLayer(LAYER_UI_BACKGROUND);
+            fillImage->SetParent(this);
+            hLayout->AddObject(fillImage, 0, j);
+        }
 
-        hLayout->AddObject(fillImage, 0, i);
+        for (int i = 0; i < playerCount; i++)
+        {
+            // Compteur des dégats
+            font = assets->GetFont(FONT_DAMAGE);
+            UIText* text = new UIText(scene, "0%", font, Colors::White);
+            text->SetAnchor(Anchor::CENTER);
 
-        // Compteur des dégats
-        font = assets->GetFont(FONT_DAMAGE);
-        UIText *text = new UIText(scene, "0%", font, Colors::White);
-        text->SetAnchor(Anchor::CENTER);
+            hLayout->AddObject(text, 0, i * 2);
 
-        hLayout->AddObject(text, 0, i);
+            m_damageTexts.push_back(text);
+        }
+        break;
 
-        m_damageTexts.push_back(text);
+    case StageConfig::Mode::LIMITED_TIME: break;
     }
 
     // Compteur du temps restant
@@ -81,7 +113,7 @@ void UIStageHUD::Update()
 {
     UIObject::Update();
 
-    StageManager *stageManager = StageManager::GetFromScene(m_scene);
+    StageManager* stageManager = StageManager::GetFromScene(m_scene);
     const int playerCount = g_gameCommon.playerCount;
 
     switch (g_gameCommon.stageConfig.mode)
